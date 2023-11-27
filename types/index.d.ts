817a0b70f65921ef393621fa980c20252709fb4d
KrisTheNewest <61835777+KrisTheNewest@users.noreply.github.com>
2023-11-28 00:42:10 +0100

    types test

---
+++ types/index.d.ts
@@ -0,0 +1,13 @@
+export function twitter(handle: String, cookies: Object[], date?: Date) : {
+    fullProfile: {
+        name: string;
+        handle: string;
+        url: string;
+        avatar: string;
+    };
+    postUrl: string;
+    postDate: string;
+    postText: string;
+    images: string[];
+    videoUrl: string | null;
+}
