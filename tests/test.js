1d33310d7e0c0330e2f3aaa84c552d1ebadea221
KrisTheNewest <61835777+KrisTheNewest@users.noreply.github.com>
2023-11-30 21:33:44 +0100

    test update

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,8 @@
 import twitterFeed from "../src/twitter.js";
-import allCookies from './../cookies.json' assert { type: 'json' }; 
+import allCookies from './../cookies.json' assert { type: 'json' };
+import time from './../time.json' assert { type: 'json' };
 
-twitterFeed("ArknightsEN", allCookies)
+twitterFeed("ArknightsEN", allCookies, time, new Map())
 	.then(arr => {
 		console.log(arr);
 	})
